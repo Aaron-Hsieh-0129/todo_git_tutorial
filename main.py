096428751d8c0b82ffb2a6bff87a386e0a5aacc5
Aaron Hsieh <zz85721@gmail.com>
2024-09-12 11:01:02 +0800

    Implement main loop function to get user input and make todo for different inputs

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+def main():
+    while True:
+        print("\n To-Do List Menu:")
+        print("1. Add a task")
+        print("2. View tasks")
+        print("3. Delete a task")
+        print("4. Exit")
+
+        choice = input("Choose an option: ")
+
+        if chioce == '1':
+            # TODO: add_task
+            pass
+        elif choice == '2':
+            # TODO: view_task
+            pass
+        elif choice == '3':
+            # TODO: delete_task
+            pass
+        elif choice == '4':
+            print("Goodbye")
+        else:
+            print("Invalid choice. Please choose a valid option.")
+
 if __name__ == "__main__":
     pass
